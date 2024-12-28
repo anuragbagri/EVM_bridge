79fdeb0471509a60fafb88a7c1076e8d807ad732
anurag bagri <anurag123bagri@gmail.com>
2024-12-28 09:54:14 +0530

    test for rupee added

--- test/Rupee.t.sol
+++ test/Rupee.t.sol
@@ -3,14 +3,20 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
-import "src/Rupee.sol";
+import "..src/Rupee.sol";
 
-contract TestContract is Test {
+contract Rupee is Test {
     Rupee c;
+    uint256  amount = 1000;
 
     function setUp() public {
         c = new Rupee();
     }
 
-    
+    function testMint() public {
+      vm.prank(msg.sender);
+      c.mint(amount);
+      assertEq(c.balanceOf(msg.sender), 1000 , "ok");
+
+    }
 }
